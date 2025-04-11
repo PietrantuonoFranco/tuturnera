049c52576f1a2dc7f36c8156cc3921f3df669304
Franquinios <francopietrantuono999@gmail.com>
2025-04-11 13:11:30 -0300

    feat: Add login and axios methods to auth

---
+++ frontend/src/api/auth.ts
@@ -0,0 +1,39 @@
+import api from "./api.ts"
+
+const entity: string = "auths";
+
+export const login = async (email: string, password: string) => {
+  const response = await api.post(`${entity}/login`, { 
+    email, 
+    password 
+  }, {
+    withCredentials: true,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+    
+  return response.data;
+};
+  
+export const logout = async () => {
+  await api.post(`${entity}/logout`, {}, {
+    withCredentials: true
+  });
+};
+
+export const getCurrentUser = async () => {
+  try {
+    const response = await api.get(`${entity}/profile`, {
+      withCredentials: true,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    return response.data.user;
+  } catch (error) {
+    console.error('Error al obtener usuario:', error);
+    
+    return null;
+  }
+};
